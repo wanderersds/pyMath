0a4bc0b8c943d9f8cfb7920bfd0ab12295911b8d
Dan Greben <SDSWanderer@gmail.com>
2012-02-22 19:44:01 +0200

    Рефакт         modified:   chordM.py 	modified:   function.py 	modified:   tryM.py

--- chordM.py
+++ chordM.py
@@ -1,33 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import division
 import string
 from function import *
 
-#formula = str(raw_input("Function: "))
-print "Function = "
-formula = str(raw_input())
-print "Epsilon = "
-E = float(input())
-print "Range:"
-print "A = "
-a = float(input())
-print "B = "
-b = float(input())
 while (True):
-
-    Fa = Function(a,'F(a)=',formula)
-    Fb = Function(b,'F(b)=',formula)
-    c_begin = c2Function(a, b, Fa, Fb)
-    Fc = Function(c_begin.value, 'F(c)=', formula)
+    Fa = Function('F(a)=', formula, ['x'], [a])
+    Fb = Function('F(b)=', formula, ['x'], [b])
+    c_begin = Function('C=', 'a-((b-a)/(Fb-Fa))*Fa', ['Fa', 'Fb', 'a', 'b'], [Fa.value, Fb.value, a, b])
+    Fc = Function('F(c)=', formula, ['x'], [c_begin.value])
     if (Fc.value > 0 and Fa.value > 0) or (Fc.value < 0 and Fa.value < 0):
-        a=c_begin.value
-        print "F(c) and F(a) having the same sign. (A=C)"
+        a = c_begin.value
+        print "F(c) и F(a) имеют один знак. (A=C)"
     else:
-        b=c_begin.value
-        print "F(c) and F(a) having the sme sign. (B=C)"
-    c_end = c2Function(a, b, Fa, Fb)
+        b = c_begin.value
+        print "F(c) и F(b) имеют один знак. (B=C)"
+    c_end = Function('C=', 'a-((b-a)/(Fb-Fa))*Fa', ['Fa', 'Fb', 'a', 'b'], [Fa.value, Fb.value, a, b])
     print "Delta(c)=" + '|' + str(c_begin.value) + '-' + str(c_end.value) + '|=' + str(abs(c_begin.value-c_end.value))
     if (abs(c_begin.value - c_end.value) <= E): break
 
--- function.py
+++ function.py
@@ -3,24 +3,42 @@
 
 from commands import getoutput
 
+formula = str(raw_input("Function = \n"))
+E = float(raw_input("Epsilon = \n"))
+print "Range:"
+a = float(raw_input("A = \n"))
+b = float(raw_input("B = \n"))
+
 class Function():
     formula = ''
     value = 0.0
-    def __init__(self, x, name, formula):
-        self.formula = formula.replace('x', str(x))
+    def __init__(self, name, formula, replace, vars):
+        self.formula = formula
+        i = 0
+        while i < len(replace):
+            self.formula = self.formula.replace(replace[i], str(vars[i]))
+            i = i + 1
         self.value = float(eval(self.formula))
         print name + self.formula + '=' + str(self.value)
 
 
-class cFunction(Function):
-    def __init__(self, a, b):
-        self.formula = str(a) + '+(' + str(b) + '-' + str(a) + ')/2.0'
-        self.value = a+(b-a)/2.0
-        print "C=" + self.formula + '=' + str(self.value)
 
-class c2Function(Function):
 
-    def __init__(self, a , b, Fa, Fb):
-        self.value = (a-((b-a)/(Fb.value-Fa.value))*Fa.value)
-        self.formula = str(a) + '-((' + str(b) + '-' + str(a) + ')/(' + str(Fb.value) + '-' + str(Fa.value) + '))*' + str(Fa.value)
-        print "C=" + self.formula + '=' + str(self.value)
+#a=b=1
+#c = Function('C=', 'a+(b-a)/2.0', ['a'], [b])
+#
+#class cFunction(Function):
+#    def __init__(self, a, b):1111
+#        self.formula = str(a) + '+(' + str(b) + '-' + str(a) + ')/2.0'
+#        self.value = a+(b-a)/2.0
+#        print "C=" + self.formula + '=' + str(self.value)
+
+#class c2Function(Function):
+#
+#    def __init__(self, a , b, Fa, Fb):
+#        self.value = (a-((b-a)/(Fb.value-Fa.value))*Fa.value)
+#        self.formula = str(a) + '-((' + str(b) + '-' + str(a) + ')/(' + str(Fb.value) + '-' + str(Fa.value) + '))*' + str(Fa.value)
+#        print "C=" + self.formula + '=' + str(self.value)
+#
+#        a-((b-a)/(Fb-Fa))*Fa
+#

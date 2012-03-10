afe79b6d52bd94a1b0155b36098c2a0041ddded9
Dan Greben <SDSWanderer@gmail.com>
2012-03-10 15:02:14 +0200

    modified:   function.py - Разработан класс Function(), в него добавлены методы перегружаюшие математику и логику.

--- function.py
+++ function.py
@@ -10,6 +10,10 @@ b = float(raw_input("B = \n"))
 class Function:
     formula = ''
     value = 0.0
+
+    def formulate(self, symbol, other):
+        self.formula = '(' + self.formula + ')' + symbol + '(' + other.formula + ')'
+
     def __init__(self, name, formula, replace, vars):
         self.formula = formula
         i = 0
@@ -18,6 +22,44 @@ class Function:
             i = i + 1
         self.value = float(eval(self.formula))
         print name + self.formula + '=' + str(self.value)
+
+    def __float__(self):
+        return self.value
+
+    def __add__(self, other):
+        self.formulate('+', other)
+        self.value = self.value + other.value
+
+    def __sub__(self, other):
+        self.formulate('-', other)
+        self.value = self.value - other.value
+
+    def __mul__(self, other):
+        self.formulate('*', other)
+        self.value = self.value * other.value
+
+    def __div__(self, other):
+        self.formulate('/', other)
+        self.value = self.value / other.value
+
+    def __lt__(self, other):
+        return self.value < other.value
+
+    def __le__(self, other):
+        return self.value <= other.value
+
+    def __eq__(self, other):
+        return self.value == other.value
+
+    def __ne__(self, other):
+        return self.value != other.value
+
+    def __gt__(self, other):
+        return self.value > other.value
+
+    def __ge__(self, other):
+        return self.value >= other.value
+
 
 def dcount(var):
     rest = var % 1
